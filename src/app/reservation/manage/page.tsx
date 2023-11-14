8dee1575ae4a1df2ebc99d07e504c371a01c5539
ArmerZ <50855686+armernuntiya@users.noreply.github.com>
2023-11-14 19:23:26 +0700

    latest update

---
+++ src/app/reservation/manage/page.tsx
@@ -0,0 +1,9 @@
+import ReservationPanel from "@/components/ReservationPanel"
+
+export default function ManagePage(){
+    return(
+    <main className="bg-gray-200">
+        <ReservationPanel/>
+    </main>
+    )
+}
---
+++ src/components/ReservationPanel.tsx
@@ -0,0 +1,11 @@
+export default function ReservationPanel(){
+    return(
+    <main className="bg-white h-full w-full">
+        <div>
+            <div>Restaurant</div>
+            <div>Date</div>
+            <div>Guest</div>
+        </div>
+        <hr/>    
+    </main>)
+}
